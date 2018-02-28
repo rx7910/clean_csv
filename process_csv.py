85fdeb4bbdfde9ea5983cd0ddb98705a4c4d72a7
rx <jiafa00000@gmail.com>
2018-02-28 10:39:43 +0800

    20180227

--- process_csv.py
+++ process_csv.py
@@ -43,6 +43,7 @@ with open('./raw/imooc-20180222.csv') as f:
                 continue
 
             w_csv.writerow(row)
+            cached_dist.append(row)
 
     w.close()
 
@@ -50,41 +51,64 @@ f.close()
 
 # 按照课程id、用户名进行排序
 
-with open('./target/out_step_1.csv') as f2:
-    with open('./target/out_step_2.csv', 'w') as w2:
-        f_csv2 = csv.reader(f2)
-        w_csv2 = csv.writer(w2)
-        f_sorted = sorted(f_csv2, key=lambda x: (x[3], x[0]))
-        for row in f_sorted:
-            w_csv2.writerow(row)
-    w2.close()
-f2.close()
+cached_dist.sort(key=lambda x: (x[3], x[0]))
+
+current_course_id = None
+next_course_id = None
+current_course_name = None
+cached_str = ''
+
+with open('./target/out_step_3.csv', 'w') as w_combine:
+    w_csv_combine = csv.writer(w_combine)
+    w_csv_combine.writerow(head)
+
+    for row in cached_dist:
+        if current_course_id == row[-2]:
+            cached_str += row[1]
+        else:
+            w_csv_combine.writerow([current_course_id, current_course_name, cached_str])
+            current_course_id = row[-2]
+            current_course_name = row[-1]
+            cached_str = row[1]
+
+w_combine.close()
+
+
+# with open('./target/out_step_1.csv') as f2:
+#     with open('./target/out_step_2.csv', 'w') as w2:
+#         f_csv2 = csv.reader(f2)
+#         w_csv2 = csv.writer(w2)
+#         f_sorted = sorted(f_csv2, key=lambda x: (x[3], x[0]))
+#         for row in f_sorted:
+#             w_csv2.writerow(row)
+#     w2.close()
+# f2.close()
 
 # 合并同一门课程下面的评论
 
-with open('./target/out_step_2.csv') as f_combine:
-    with open('./target/out_step_3.csv', 'w') as w_combine:
-        f_csv_combine = csv.reader(f_combine)
-        w_csv_combine = csv.writer(w_combine)
-
-        print('headhadfalsdkjflaskdjfalskjdfalksdfjasdlf===============!!!!!!!!!!!!!!!', head)
-        w_csv_combine.writerow(head)
-
-        current_course_id = None
-        next_course_id = None
-        current_course_name = None
-        cached_str = ''
-
-        for row in f_csv_combine:
-            if current_course_id == row[-2]:
-                cached_str += row[1]
-            else:
-                w_csv_combine.writerow([current_course_id, current_course_name, cached_str])
-                current_course_id = row[-2]
-                current_course_name = row[-1]
-                cached_str = row[1]
-
-    w_combine.close()
-
-f_combine.close()
+# with open('./target/out_step_2.csv') as f_combine:
+#     with open('./target/out_step_3.csv', 'w') as w_combine:
+#         f_csv_combine = csv.reader(f_combine)
+#         w_csv_combine = csv.writer(w_combine)
+#
+#         print('headhadfalsdkjflaskdjfalskjdfalksdfjasdlf===============!!!!!!!!!!!!!!!', head)
+#         w_csv_combine.writerow(head)
+#
+#         current_course_id = None
+#         next_course_id = None
+#         current_course_name = None
+#         cached_str = ''
+#
+#         for row in f_csv_combine:
+#             if current_course_id == row[-2]:
+#                 cached_str += row[1]
+#             else:
+#                 w_csv_combine.writerow([current_course_id, current_course_name, cached_str])
+#                 current_course_id = row[-2]
+#                 current_course_name = row[-1]
+#                 cached_str = row[1]
+#
+#     w_combine.close()
+#
+# f_combine.close()
 
